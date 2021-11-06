8fcf20b301c6d9cf4e693699c69ba758d6cd9ea5
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-11-06 10:13:07 +0300

    Veritipleri tekrar ve anonim tipler

---
+++ Variables6Tekrar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Variables6Tekrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ön tanımlı veritipleridir..
+            // sbyte, byte,short,ushort, int, uint,long,ulong
+            // double, decimal, float
+
+            // string
+            // char (tek karakter)
+
+            //bool (true,false)
+
+            // value type(değer tipler) => sadece değer tutan tiplerdir...
+            // sbyte, byte,short,ushort, int, uint,long,ulong, double, decimal, float, char, bool
+
+
+            // reference type(referans tipler) => değer yerine refarans tutan tiplerdir...
+            //string (birden fazla karakterin referansını tutar....
+
+            int de = 3;
+            int a = de;
+            de++;
+            Console.WriteLine(a); // 3,
+
+            byte b = 3;
+            int i = 200000;
+
+            double d = 3.1; // yorumlayıcımız 3.1'i double olarak yorumlar.. eğer kullanacağımız değer double dışında bir tip olacaksa değer atandığında float ve decimal'ın arka ekini kullanmamız gerekir..
+            float j = 3.1f;
+            decimal k = 3.1m;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Variables7AnonymouesTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables7AnonymouesTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Anonim tipler
+
+            // var ifadelerinin özelliği atana değerin tipini alır..
+
+            var ad = "ekrem yıldırım"; // string
+            var cinsiyet = 'e'; // char
+            var cocuk = true; // bool
+            var maas = 1000.99; // double
+            var ortalama = 50.1f; // float
+
+            // cocuk ="alp" // değişkeniminiz tipi bool olduğu için string  değer atanamaz...
+
+            // var çok sık kullanılan bir tiptir. Nedeni anonim olmasıdır. object tabanlı uygulamalarda anlık çözümler için kullanılır...
+
+
+            // var'da tip değer atandığında oluştuğu için başlangıç değerinin atanması zorunludur...
+            //var ders;
+
+            // eğer değişkeni oluşuturup değer atanmadan kullanamak isteniyorsa aşağıdaki gibi kullanılabilir...
+        
+            string s;
+            var ders = default(string);
+            
+        }
+    }
+}
